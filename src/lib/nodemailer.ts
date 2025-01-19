941cd253af055f8ccef6ddf762673db15d323a4c
Subhadeep <codevizag@gmail.com>
2025-01-19 07:10:02 +0530

    Fixed Mail Issue 👀

--- src/lib/nodemailer.ts
+++ src/lib/nodemailer.ts
@@ -1,9 +1,9 @@
 import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
-  host: 'email-smtp.ap-south-1.amazonaws.com',
+  host: 'smtp.zoho.in',
   port: 587, // Use 587 if you want to use TLS
-  secure: true, // Set to true if you're using SSL (port 465)
+  secure: false, // Set to true if you're using SSL (port 465)
   auth: {
     user: process.env.EMAIL_USER,  
     pass: process.env.EMAIL_PASS,  
@@ -13,7 +13,7 @@ const transporter = nodemailer.createTransport({
 export const sendEmail = async (to: string, subject: string, html: string) => {
   try {
     await transporter.sendMail({
-      from: process.env.EMAIL_FROM, // Sender email
+        from: `"Music by Illuzon" <${process.env.FROM_EMAIL}>`, // Sender email
       to,
       subject,
       html,
